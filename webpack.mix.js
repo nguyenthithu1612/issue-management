4ad99ff098634bdb1dc7fddcf7320fe4681ddbf4
samxinhgai <t1505g.ntanh@aptech.vn>
2019-10-19 08:04:05 +0700

    add adminlte

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,11 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+mix.styles('node_modules/admin-lte/bower_components/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
+mix.styles('node_modules/admin-lte/bower_components/font-awesome/css/font-awesome.min.css', 'public/css/font-awesome.min.css');
+mix.styles('node_modules/admin-lte/bower_components/Ionicons/css/ionicons.min.css', 'public/css/ionicons.min.css');
+mix.styles('node_modules/admin-lte/dist/css/AdminLTE.min.css', 'public/css/AdminLTE.min.css');
+mix.styles('node_modules/admin-lte/dist/css/skins/skin-blue.min.css', 'public/css/skin-blue.min.css');
+mix.scripts('node_modules/admin-lte/bower_components/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
+mix.scripts('node_modules/admin-lte/bower_components/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
+mix.scripts('node_modules/admin-lte/dist/js/adminlte.min.js', 'public/js/adminlte.min.js');
